7e95a73d1e70456e598736f1569ebfaa4a38ea28
Rishab Gupta <risgupta3@publcisgroupe.net>
2021-04-28 22:31:07 +0530

    added role

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          New app again again
+          New app again
         </a>
       </header>
     </div>
