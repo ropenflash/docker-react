cab8bb4f1c19beaed0876081daf40875973b6e8b
Rishab Gupta <risgupta3@publcisgroupe.net>
2021-04-28 22:50:06 +0530

    last check

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          New app agai
+          New app again
         </a>
       </header>
     </div>
